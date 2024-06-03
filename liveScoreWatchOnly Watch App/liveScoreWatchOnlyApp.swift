9a219bd97a0a4d146e915c421f90e2344a7e15af
Pablo <pabloubeda2000@gmail.com>
2024-06-03 10:05:07 +0200

    Initial Commit

---
+++ liveScoreWatchOnly Watch App/liveScoreWatchOnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  liveScoreWatchOnlyApp.swift
+//  liveScoreWatchOnly Watch App
+//
+//  Created by Pablo Fernandez Gonzalez on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct liveScoreWatchOnly_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
