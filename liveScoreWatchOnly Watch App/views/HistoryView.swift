1b352a28088500cf1f1b7c2997f5349913030d10
Pablo <pabloubeda2000@gmail.com>
2024-06-09 18:09:53 +0200

    added option to remove match from history when longpressed

--- liveScoreWatchOnly Watch App/views/HistoryView.swift
+++ liveScoreWatchOnly Watch App/views/HistoryView.swift
@@ -14,79 +14,101 @@ struct HisoryView: View{
     @Environment(\.modelContext) var context
     @Query (sort: \TennisMatch.timestamp, order: .reverse) private var matches: [TennisMatch]
     
+    @State private var showAlert = false
+    @State private var matchToDelete: TennisMatch?
+    
     var body: some View {
-        List(matches) { match in
-            /*NavigationLink("\(match.player1.name) - \(match.player1Sets)\n\(match.player2.name) - \(match.player2Sets)", value: match)
-                .navigationDestination(for: TennisMatch.self){ match in
-                    MatchView(match: match)
-                }*/
-            NavigationLink {
-                MatchView(match: match,commingFromConfig: false)
-            } label:{
-                HStack{
-                    VStack{
+        if(matches.isEmpty){
+            Text("No matches yet")
+        }else{
+            List{
+                ForEach(matches) { match in
+                    NavigationLink {
+                        MatchView(match: match,commingFromConfig: false)
+                    } label:{
                         HStack{
-                            Text("\(match.player1.name)")
-                                .colorMultiply(match.winner.name==match.player1.name ? .green : .primary)
-                                .frame(width: 50, height: 20, alignment: .trailing)
-                                .fixedSize(horizontal: true, vertical: true)
-                            Divider()
-                            HStack{
-                                Text("\(match.player1Sets)").frame(width: 10, alignment: .center)
-                                if(!match.isMatchFinished){
+                            VStack{
+                                HStack{
+                                    Text("\(match.player1.name)")
+                                        .colorMultiply(match.winner.name==match.player1.name ? .green : .primary)
+                                        .frame(width: 50, height: 20, alignment: .trailing)
+                                        .fixedSize(horizontal: true, vertical: true)
                                     Divider()
-                                    Text("\(match.player1Games)").frame(width: 10, alignment: .center)
-                                    Divider()
-                                    if(currentScore(match.player1Points)=="ADV"){
-                                        Text(currentScore(match.player1Points)).frame(width: 30, alignment: .center).font(.system(size: 10))
-                                    }else{
-                                        Text(currentScore(match.player1Points)).frame(width: 30, alignment: .center)
-                                    }
-                                    Image(systemName: "tennisball")
-                                        .opacity(match.server == .player1 ? 1:0)
-                                        .font(.system(size: 10))
-                                }
-                            }.frame(alignment: .leading).fixedSize(horizontal: true, vertical: true)
-                        }
-                        HStack{
-                            Text("\(match.player2.name)")
-                                .colorMultiply(match.winner.name==match.player2.name ? .green : .primary)
-                                .frame(width: 50, height: 20, alignment: .trailing)
-                                .fixedSize(horizontal: true, vertical: true)
-                            Divider()
-                            HStack{
-                                Text("\(match.player2Sets)").frame(width: 10, alignment: .center)
-                                if(!match.isMatchFinished){
-                                    Divider()
-                                    Text("\(match.player2Games)").frame(width: 10, alignment: .center)
-                                    Divider()
-                                    if(match.isTieBreak){
-                                        Text("\(match.player2TieBreakPoints)").frame(width: 30, alignment: .center)
-                                    }else{
-                                        if(currentScore(match.player2Points)=="ADV"){
-                                            Text(currentScore(match.player2Points)).frame(width: 30, alignment: .center).font(.system(size: 10))
-                                        }else{
-                                            Text(currentScore(match.player2Points)).frame(width: 30, alignment: .center)
+                                    HStack{
+                                        Text("\(match.player1Sets)").frame(width: 10, alignment: .center)
+                                        if(!match.isMatchFinished){
+                                            Divider()
+                                            Text("\(match.player1Games)").frame(width: 10, alignment: .center)
+                                            Divider()
+                                            if(currentScore(match.player1Points)=="ADV"){
+                                                Text(currentScore(match.player1Points)).frame(width: 30, alignment: .center).font(.system(size: 10))
+                                            }else{
+                                                Text(currentScore(match.player1Points)).frame(width: 30, alignment: .center)
+                                            }
+                                            Image(systemName: "tennisball")
+                                                .opacity(match.server == .player1 ? 1:0)
+                                                .font(.system(size: 10))
                                         }
-                                    }
-                                    Image(systemName: "tennisball")
-                                        .opacity(match.server == .player2 ? 1:0)
-                                        .font(.system(size: 10))
+                                    }.frame(alignment: .leading).fixedSize(horizontal: true, vertical: true)
                                 }
-                            }.frame(alignment: .leading).fixedSize(horizontal: true, vertical: true)
+                                HStack{
+                                    Text("\(match.player2.name)")
+                                        .colorMultiply(match.winner.name==match.player2.name ? .green : .primary)
+                                        .frame(width: 50, height: 20, alignment: .trailing)
+                                        .fixedSize(horizontal: true, vertical: true)
+                                    Divider()
+                                    HStack{
+                                        Text("\(match.player2Sets)").frame(width: 10, alignment: .center)
+                                        if(!match.isMatchFinished){
+                                            Divider()
+                                            Text("\(match.player2Games)").frame(width: 10, alignment: .center)
+                                            Divider()
+                                            if(match.isTieBreak){
+                                                Text("\(match.player2TieBreakPoints)").frame(width: 30, alignment: .center)
+                                            }else{
+                                                if(currentScore(match.player2Points)=="ADV"){
+                                                    Text(currentScore(match.player2Points)).frame(width: 30, alignment: .center).font(.system(size: 10))
+                                                }else{
+                                                    Text(currentScore(match.player2Points)).frame(width: 30, alignment: .center)
+                                                }
+                                            }
+                                            Image(systemName: "tennisball")
+                                                .opacity(match.server == .player2 ? 1:0)
+                                                .font(.system(size: 10))
+                                        }
+                                    }.frame(alignment: .leading).fixedSize(horizontal: true, vertical: true)
+                                }
+                            }
+                            Spacer()
+                            VStack{
+                                if(match.isMatchFinished){
+                                    Label("", systemImage: "info.circle").colorMultiply(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                }else{
+                                    Label("",systemImage: "chevron.right.circle").colorMultiply(.green)
+                                }
+                            }.padding(.trailing, 8)
                         }
+                    }.onLongPressGesture(minimumDuration: 0.5) {
+                        self.matchToDelete = match
+                        self.showAlert = true
                     }
-                    Spacer()
-                    VStack{
-                        if(match.isMatchFinished){
-                            Label("", systemImage: "info.circle").colorMultiply(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                        }else{
-                            Label("",systemImage: "chevron.right.circle").colorMultiply(.green)
-                        }
-                    }.padding(.trailing, 8)
+                    .navigationTitle("Match history")
                 }
+            }.alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("\(matchToDelete!.player1.name) - \(matchToDelete!.player2.name)"),
+                    message: Text("Are you sure you want to delete this match?"),
+                    primaryButton: .destructive(Text("Delete")) {
+                        if let matchToDelete = matchToDelete {
+                            context.delete(matchToDelete)
+                        }
+                    },
+                    secondaryButton: .cancel(Text("Cancel")){
+                        matchToDelete = nil
+                        showAlert = false
+                    }
+                )
             }
-            .navigationTitle("Match history")
         }
     }
     private func currentScore(_ points: Int) -> String {
